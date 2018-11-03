6a42aa80ac3b05d77a588585785d36f9363cda94
Shahzaib Ali <confuzedone@Shahzaibs-MacBook-Air.local>
2018-11-03 16:55:47 -0500

    Converted db from sqlite3 to peewee

--- backend/restaurants_db.py
+++ backend/restaurants_db.py
@@ -1,26 +1,5 @@
 import peewee
 from peewee import *
- 
-"""
-def create_database():
-    conn = sqlite3.connect("restaurants.db")
-
-    c = conn.cursor()
-    c.execute('''CREATE TABLE restaurants
-        (restaurant_name text, longitude real, latitude real, address text, about text)''')
-    c.execute('''CREATE TABLE menu
-        (restaurant_name text, longitude real, latitude real, address text, about text)''')    
-    conn.commit()
-    conn.close()
-
-def update_database():
-    conn = sqlite3.connect("restaurants.db")
-    c = conn.cursor()
-    conn.commit()
-    conn.close()
-
-create_database()
-"""
 
 db = SqliteDatabase('restaurants.db')
 
