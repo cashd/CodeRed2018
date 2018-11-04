f65add1c2b8e731d3e85bef3fd3f317469369578
Cash DeLeon <cadeleon2@uh.edu>
2018-11-03 21:11:14 -0500

    started finding closest rest

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+from backend.database import get_rest_in_range
 
 
 app = Flask(__name__)
@@ -11,6 +12,14 @@ def home():
 
 @app.route("/api/sendDetails", methods=['POST'])
 def handleRequest():
-    print(request.json)
+    user_data = request.get_json()
+    #if user_data['keywords'] and user_data['range'] and user_data['position']:
+    keywords = user_data['keywords']
+    range = int(user_data['range'])
+    position = user_data['position']
+    r = get_rest_in_range(float(position['lat']), float(position['long']), range)
+    print(r)
+    #r = get_rest_in_range()
+    #print(r)
     data = { "hi": 4 }
     return jsonify(data)
