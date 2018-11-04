deb5e48a83c35d40db6c8412f9ddc9ab57ebefc3
Cash DeLeon <cadeleon2@uh.edu>
2018-11-03 19:42:57 -0500

    working api, fixed cx origin flask thing

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-
 @app.route("/api/sendDetails", methods=['POST'])
 def handleRequest():
-    return request.data
+    print(request.json)
+    data = { "hi": 4 }
+    return jsonify(data)
