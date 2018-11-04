c72212d2147fa4c15a98495acb96802620eeece3
Joel Jijo <JoelJ@Joels-MacBook-Pro.local>
2018-11-03 19:18:23 -0500

    PizzaHut

--- backend/restaurants_db.py
+++ backend/restaurants_db.py
@@ -15,17 +15,25 @@ class Restaurants(Base):
     about = CharField()
 
 class Menu(Base):
-    id = ForeignKeyField(Restaurants,on_delete='CASCADE')
-    restaurant_name=CharField()
+    restaurant_name = ForeignKeyField(Restaurants,on_delete='CASCADE')
     menu_item = CharField()
     price = DoubleField()
+
+
 
 def populate_test_data():
     db.create_tables([Restaurants, Menu])
     #for i in range(10):
-    Restaurants.create(name='pizza hut',longitude= 2.6, latitude=3.6,address= 'Enchanted',about='Spring')
+    Restaurants.create(name='Pizza Hut',longitude= 2.6, latitude=3.6,address= 'Enchanted',about='Spring')
+    Restaurants.create(name='Dominoes',longitude= 2.6, latitude=3.6,address= 'Enchanted',about='Spring')
+    Restaurants.create(name='Pizza Hut',longitude= 2.5, latitude=4.6,address= 'Enchanted',about='Spring')
     # for i in range(10):
-    Menu.create(restaurant_name='pizza hut',menu_item="pizza", price=5.5)
+    Menu.create(restaurant_name=id,menu_item='pizza', price=5.5)
+    Menu.create(restaurant_name=Restaurants.get(name="Pizza Hut"),menu_item='wings', price=10.5)
+    Menu.create(restaurant_name=Restaurants.get(name="Pizza Hut"),menu_item='pasta', price=6)
+    Menu.create(restaurant_name=Restaurants.get(name="Dominoes"),menu_item='pizza', price=5.5)
+    Menu.create(restaurant_name=Restaurants.get(name="Dominoes"),menu_item='wings', price=10.5)
+    Menu.create(restaurant_name=Restaurants.get(name="Dominoes"),menu_item='pasta', price=6)
 
 db.connect()
 populate_test_data()
