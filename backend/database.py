cca2bf8ad96140de9f5871babc46d37275070935
Joel Jijo <JoelJ@Joels-MacBook-Pro.local>
2018-11-04 01:15:45 -0500

    db is fixed with variables

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from peewee import *
 import csv
 from geopy import distance
 
-db = SqliteDatabase('/home/cash/Documents/CodeRed/CodeRed2018/backend/restaurants.db')
+db = SqliteDatabase('restaurants.db')
 
 class Base(Model):
     class Meta:
@@ -14,6 +14,7 @@ class Restaurants(Base):
     latitude = FloatField()
     address = CharField()
     info = CharField()
+    hours = CharField()
     link = CharField()
 
 class Menu(Base):
@@ -21,14 +22,13 @@ class Menu(Base):
     menu_item = CharField()
     price = DoubleField()
     info = CharField()
-    catagory = CharField()
+    category = CharField()
 
 
 def create_database():
     db.create_tables([Restaurants, Menu])
 
 def populate_test_data():
-    db.connect()
     db.create_tables([Restaurants, Menu])
 
     with open('restaurants.csv') as csv_file:
@@ -36,19 +36,19 @@ def populate_test_data():
         restaurant_start=True
         for row in csv_reader: 
             if restaurant_start == True:
-                rest=Restaurants.create(name=row[0],longitude= row[2], latitude=row[3],address= row[1],about=row[4])
+                rest=Restaurants.create(name=row[0],address= row[1], longitude= row[2], latitude=row[3],info=row[4],hours=row[5],link=row[6])
                 rest.save
                 restaurant_start=False
             elif row[0]=='':
                 restaurant_start=True
             else:
-                men=Menu.create(restaurant_name=rest.id,menu_item=row[0], price=row[1])
+                men=Menu.create(restaurant=rest.id,menu_item=row[0], price=row[1], info=row[2], category=row[3])
                 men.save
 
 
 
 # Gets all restraunt rows that are in range of the user
-def get_rest_in_range(user_lat, user_long, max_range):
+'''def get_rest_in_range(user_lat, user_long, max_range):
     db.connect()
     restaurants = Restaurants.select()
     print(len(restaurants))
@@ -62,3 +62,6 @@ def get_rest_in_range(user_lat, user_long, max_range):
             r_in_range.append(r.name)
 
     return r_in_range
+'''
+create_database()
+populate_test_data()
