2707d0a3c3140f32c90ebc388ffa8c741c6e4255
Shahzaib Ali <confuzedone@Shahzaibs-MacBook-Air.local>
2018-11-04 01:50:21 -0600

    added function to convert coordinates to an address

--- backend/db_service.py
+++ backend/db_service.py
@@ -14,3 +14,14 @@ def convert_address(location):
     longitude = geocoding_json['results'][0]['geometry']['location']['lng']
 
     return longitude, latitude
+
+def convert_coordinates(longitude, latitude):
+    api_key = "AIzaSyA71w-QBNhPJfqVovLnhskJbgtHhHFwuaI"
+
+    geocoding_url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + longitude + ',' + latitude + '&key=' + api_key
+    geocoding_data = requests.get(geocoding_url)
+    geocoding_json = json.loads(geocoding_data)
+
+    address = geocoding_json['results'][0]['formatted_address']
+
+    return address
